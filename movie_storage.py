7f7195c59b593babd56cdde23c0712670df3e27e
Inga355 <albi1303@gmail.com>
2024-11-15 15:08:10 +0100

    rename data.json to movies.json, deleted some unnecessary description, added space between functions

--- movie_storage.py
+++ movie_storage.py
@@ -5,7 +5,7 @@ FILENAME = 'movies.json'
 
 def get_movies():
     """
-    Returns a dictionary of dictionaries that
+    Returns a dictionary of lists that
     contains the movies information in the database.
 
     The function loads the information from the JSON
@@ -18,30 +18,33 @@ def get_movies():
     except FileNotFoundError:
         return {}
 
+
 def save_movies(movies):
     """
-    Gets all your movies as an argument and saves them to the JSON file.
+    Gets all movies as an argument and saves them to the JSON file.
     """
     with open(FILENAME, 'w') as json_file:
         json.dump(movies, json_file, indent=4)
     print(f"Movies data has been saved to {FILENAME}")
 
+
 def add_movie(title, year, rating):
     """
     Adds a movie to the movies database.
-    Loads the information from the JSON file, add the movie,
-    and saves it. The function doesn't need to validate the input.
+    Loads the information from the JSON file, adds the movie
+    and saves it.
     """
     movies = get_movies()
     movies[title] = [rating, year]
     save_movies(movies)
     print(f"The movie '{title}' has been added to the database.")
 
+
 def delete_movie(title):
     """
     Deletes a movie from the movies database.
-    Loads the information from the JSON file, deletes the movie,
-    and saves it. The function doesn't need to validate the input.
+    Loads the information from the JSON file, deletes the movie
+    and saves it.
     """
     movies = get_movies()
     if title in movies:
@@ -51,11 +54,12 @@ def delete_movie(title):
     else:
         print(f"ERROR! The movie '{title}' does not exist in the database.")
 
+
 def update_movie(title, rating):
     """
     Updates a movie from the movies database.
-    Loads the information from the JSON file, updates the movie,
-    and saves it. The function doesn't need to validate the input.
+    Loads the information from the JSON file, updates the movie
+    and saves it.
     """
     movies = get_movies()
     if title in movies:
